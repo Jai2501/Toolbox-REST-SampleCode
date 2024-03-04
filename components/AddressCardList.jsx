a7c6ed8ed90aef6ba2e5afea73e75c23045f17e8
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2024-03-04 16:56:28 +0800

    Frontend - Fetch All Data

--- components/AddressCardList.jsx
+++ components/AddressCardList.jsx
@@ -1,7 +1,8 @@
 'use client'
 
 import AddressCard from './AddressCard'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 // render list of AddressCard components
 const AddressCardList = () => {
@@ -12,6 +13,21 @@ const AddressCardList = () => {
       description: '505 Independence Dr.',
     }
   ])
+
+  useEffect(() => {
+    const fetchAddresses = async () => {
+      try {
+        // make a GET request to backend server via axios
+        const response = await axios.get('http://localhost:8080/api/addresses')
+        // update the addresses state with the retrieved data
+        setAddresses(response.data) 
+      } catch (error) {
+        console.error('Error fetching addresses:', error)
+      }
+    }
+
+    fetchAddresses()
+}, [])
 
   // Function to delete an address
   const deleteAddress = (addressId) => {
@@ -25,7 +41,7 @@ const AddressCardList = () => {
   return (
     <div className="flex-1 max-w-3xl">
       {addresses.map((address) => (
-        <AddressCard key={address.id} address={address} onDelete={deleteAddress}/>
+        <AddressCard key={address._id} address={address} onDelete={deleteAddress}/>
       ))}
     </div>
   )
